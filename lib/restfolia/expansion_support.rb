70d3e62d417c14223795a4adab4c37db682515ee
Nadilson <nadilsons@gmail.com>
2012-05-25 15:39:30 -0300

    creating expansion support

---
+++ lib/restfolia/expansion_support.rb
@@ -0,0 +1,33 @@
+# 
+# Terms from RFC 6570 (URI Template) March 2012:
+# 
+#    expression:  The text between '{' and '}', including the enclosing
+#       braces, as defined in Section 2.
+# 
+#    expansion:  The string result obtained from a template expression
+#       after processing it according to its expression type, list of
+#       variable names, and value modifiers, as defined in Section 3.
+# 
+#    template processor:  A program or library that, given a URI Template
+#       and a set of variables with values, transforms the template string
+#       into a URI reference by parsing the template for expressions and
+#       substituting each one with its corresponding expansion.
+# 
+# see http://tools.ietf.org/html/rfc6570
+module Restfolia::ExpansionSupport
+
+  INVALID_PARAMETER = "parameter must to be a Hash object"
+  INVALID_URL       = "Invalid url"
+
+  def expansion(replacements)
+    raise(ArgumentError, Restfolia::ExpansionSupport::INVALID_PARAMETER, caller) unless replacements.is_a?(Hash)
+
+    # accessing hash by symbol or string key
+    replacements.inject({}) { |memo,(k,v)| memo[k.to_s] = v; memo }
+    expanded_url = URITemplate.new(@url).expand(replacements)
+
+    raise(ArgumentError, Restfolia::ExpansionSupport::INVALID_URL, caller) if (expanded_url =~ URI::regexp).nil?
+    Restfolia::EntryPoint.new(expanded_url)
+  end
+
+end
---
+++ spec/lib/restfolia/entry_point_spec.rb
@@ -0,0 +1,29 @@
+require "spec/spec_helper"
+
+describe Restfolia::EntryPoint do
+
+  context "should apply expansion with valid expression" do
+    subject { Restfolia::EntryPoint.new("http://example.com/~{username}/", "search").expansion("username" => "bug") }
+
+    its(:class) { should == Restfolia::EntryPoint     }
+    its(:url)   { should == "http://example.com/~bug/" }
+    its(:rel)   { should be_nil                       }
+    #http://example.com/dictionary/{term:1}/{term}
+  end
+
+  it "should raise exception when url is empty" do
+    ep = Restfolia::EntryPoint.new("", "search")
+    lambda { ep.expansion(:teste => false) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_URL
+  end
+
+  it "should raise exception when url is invalid" do
+    ep = Restfolia::EntryPoint.new("@@bla", "search")
+    lambda { ep.expansion(:teste => false) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_URL
+  end
+
+  it "should raise exception when params is not a valid hash" do
+    ep = Restfolia::EntryPoint.new("http://example.com/~{username}/", "search")
+    lambda { ep.expansion(:what) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_PARAMETER
+  end
+
+end

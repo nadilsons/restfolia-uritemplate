3d37308b09934349f5c45b190a27260df9705b57
Nadilson <nadilsons@gmail.com>
2012-05-28 16:54:23 -0300

    upgrade restfolia verson

--- spec/lib/restfolia/entry_point_spec.rb
+++ spec/lib/restfolia/entry_point_spec.rb
@@ -3,7 +3,7 @@ require "spec/spec_helper"
 describe Restfolia::EntryPoint do
 
   context "should apply expansion with valid expression" do
-    subject { Restfolia::EntryPoint.new("http://example.com/~{username}/", "search").expansion("username" => "bug") }
+    subject { Restfolia::EntryPoint.new("http://example.com/~{username}/", "search").expand(:username => "bug") }
 
     its(:class) { should == Restfolia::EntryPoint      }
     its(:url)   { should == "http://example.com/~bug/" }
@@ -12,17 +12,17 @@ describe Restfolia::EntryPoint do
 
   it "should raise exception when url is empty" do
     ep = Restfolia::EntryPoint.new("", "search")
-    lambda { ep.expansion(:teste => false) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_URL
+    lambda { ep.expand(:teste => false) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_URL
   end
 
   it "should raise exception when url is invalid" do
     ep = Restfolia::EntryPoint.new("@@bla", "search")
-    lambda { ep.expansion(:teste => false) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_URL
+    lambda { ep.expand(:teste => false) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_URL
   end
 
   it "should raise exception when params is not a valid hash" do
     ep = Restfolia::EntryPoint.new("http://example.com/~{username}/", "search")
-    lambda { ep.expansion(:what) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_PARAMETER
+    lambda { ep.expand(:what) }.should raise_error ArgumentError, Restfolia::ExpansionSupport::INVALID_PARAMETER
   end
 
 end
--- spec/lib/restfolia_spec.rb
+++ spec/lib/restfolia_spec.rb
@@ -5,7 +5,7 @@ describe Restfolia do
   let(:url) { "http://local.tester.anti.bug/resource/index" }
   before { register_uri(:get, url, :body => fixture('index.json')) }
 
-  subject { Restfolia.at(url).get.link('search').expansion(:term => "dog") }
+  subject { Restfolia.at(url).get.links('search').expand(:term => "dog") }
 
   its(:class) { should == Restfolia::EntryPoint                }
   its(:url)   { should == "http://local.tester.anti.bug/d/dog" }

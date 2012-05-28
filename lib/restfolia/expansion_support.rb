3a0a126aad0569ba4f7f4b52eac9df27441fcab1
Nadilson <nadilson.silva@abril.com.br>
2012-05-28 14:00:46 -0300

    adding doc in the source

--- lib/restfolia/expansion_support.rb
+++ lib/restfolia/expansion_support.rb
@@ -1,24 +1,62 @@
-# 
+#
 # Terms from RFC 6570 (URI Template) March 2012:
-# 
+#
 #    expression:  The text between '{' and '}', including the enclosing
 #       braces, as defined in Section 2.
-# 
+#
 #    expansion:  The string result obtained from a template expression
 #       after processing it according to its expression type, list of
 #       variable names, and value modifiers, as defined in Section 3.
-# 
+#
 #    template processor:  A program or library that, given a URI Template
 #       and a set of variables with values, transforms the template string
 #       into a URI reference by parsing the template for expressions and
 #       substituting each one with its corresponding expansion.
-# 
+#
 # see http://tools.ietf.org/html/rfc6570
+#
+# Public: Add uri-template supports with expansion method
+#
+# Examples
+#
+# With this json
+# { "value": 10,
+#   "name": "Test",
+#   "query": null,
+#   "tested": false,
+#   "links": [
+#     {
+#       "href": "http://local.tester.anti.bug/{term:1}/{term}",
+#       "rel": "search",
+#       "type": "application/json"
+#     },
+#     {
+#       "href": "http://local.tester.anti.bug/foo{?query,number}",
+#       "rel": "another_search",
+#       "type": "application/json"
+#     },
+#     {
+#       "href": "http://local.tester.anti.bug/resource/index",
+#       "rel": "self",
+#       "type": "application/json"
+#     }
+#   ] }
+#
+# You can do this
+#   resource = Restfolia.at(url).get.link('search').expansion(:term => "dog")
+#   resource.get  # => GET url http://local.tester.anti.bug/d/dog
+#
 module Restfolia::ExpansionSupport
 
   INVALID_PARAMETER = "parameter must to be a Hash object"
   INVALID_URL       = "Invalid url"
 
+  # Public: Execute expansion
+  #
+  # json - Hash that contains replacements params
+  #
+  # Raises ArgumentError if json parameter is not a Hash object.
+  # Raises ArgumentError if generated url is invalid.
   def expansion(replacements)
     raise(ArgumentError, INVALID_PARAMETER, caller) unless replacements.is_a?(Hash)
 
